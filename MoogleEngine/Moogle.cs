c99c109db0657da117a0af6c11d7af15982e08f1
vextroyer <victormanuel.vena@gmail.com>
2023-03-26 23:50:00 -0400

    Encapsular metodo de ordenamiento de resultados

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -6,6 +6,7 @@ public static class Moogle
     public static SearchResult Query(string query) {
         // Modifique este método para responder a la búsqueda
 
+        //Carga los documentos
         Documento[] documentos = Cargador.Load();
 
         //Crea un resultado por cada documento
@@ -14,8 +15,14 @@ public static class Moogle
             items[i] = new SearchItem(documentos[i].Titulo,documentos[i].Titulo,FrecuenciaNormalizada(query,documentos[i]));
         }
 
-        //Ordenamiento respecto al score
-        //Insertion Sort
+        //Ordenar
+        Ordenar(items);
+
+        return new SearchResult(items, query);
+    }
+
+    //Ordena los documentos descendentemente por valor de score utilizando el insertion sort
+    private static void Ordenar(SearchItem[] items){
         for(int i=0;i<items.Length;++i){
             for(int j=i;j>0 && items[j].Score > items[j - 1].Score;--j){
                 SearchItem c = new SearchItem(items[j]);
@@ -23,12 +30,10 @@ public static class Moogle
                 items[j - 1] = c;
             }
         }
-
-        return new SearchResult(items, query);
     }
 
     //La frecuencia normalizada es una variacion de la frecuencia de termino que evita una predisposicion hacia documentos largos
-    public static float FrecuenciaNormalizada(string query,Documento d){
+    private static float FrecuenciaNormalizada(string query,Documento d){
         float frecuenciaBruta = d.FrecuenciaBruta(query);
         float mayorFrecuenciaBruta = d.MostFrequentCount;
         //mayorFrecuenciaBruta = 0 nunca sucedera, porque esto implica que existe un documento vacio y de suceder esto ya la clase Cargador se hubiese encargado de ignorarlo, o que no existen documentos en la coleccion, y la clase Cargador se encargara de lanzar una excepcion en dicho caso.
