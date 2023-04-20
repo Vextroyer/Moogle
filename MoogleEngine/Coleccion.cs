0b50f64dd116c955eede4878eb82e5243dda4c87
vextroyer <victormanuel.vena@gmail.com>
2023-04-20 01:37:16 -0400

    Mejorar clase Coleccion

--- MoogleEngine/Coleccion.cs
+++ MoogleEngine/Coleccion.cs
@@ -26,13 +26,33 @@ static class Coleccion{
             _documentos = value;
         }
     }
-
     //Cantidad de documentos en la coleccion
     public static int Count{
         get{
             return _documentos.Length;
         }
     }
+
+    //Es el conjunto de terminos a partir de los cuales se crean los documentos de la coleccion
+    private static string[] _terminos = new string[0];
+    //Conjunto de terminos
+    public static string[] Terminos{
+        get{
+            return _terminos;
+        }
+        private set{
+            _terminos = value;
+        }
+    }
+    //Cardinalidad del conjunto de terminos a partir del cual se crean los documentos de esta coleccion
+    public static int UniqueTermsCount{
+        get{
+            return _terminos.Length;
+        }
+    }
+    private static Dictionary<string, List<int>> _terminosYApariciones = new Dictionary<string, List<int>>();//Relaciona el termino con los documentos en los que aparece
+    //Valor idf de cada termino del conjunto de terminos de la coleccion
+    private static double[] _idf = new double[0];
 
     //Inicializa la coleccion con los documentos dados.
     public static void Inicializar(Documento[] documentos){
@@ -42,12 +62,53 @@ static class Coleccion{
         Coleccion.Inicializada = true;
         
         Coleccion.Documentos = documentos;
+
+        //Por cada documento
+        for(int i=0;i<Coleccion.Count;++i){
+            //Asocia este documento a dicho termino
+            foreach(string termino in Coleccion.At(i).GetUniqueTerms()){
+                if(!Coleccion._terminosYApariciones.ContainsKey(termino))Coleccion._terminosYApariciones.Add(termino,new List<int>());//Si es un nuevo termino, agregalo
+                Coleccion._terminosYApariciones[termino].Add(i);//Asocia el termino con el documento donde aparece.
+            }
+        }
+        
+        Coleccion.Terminos = Coleccion._terminosYApariciones.Keys.ToArray();
+        Coleccion._idf = new double[Coleccion.UniqueTermsCount];
+        
+        //El valor idf de un termino es 
+        //idf(t,D) = log ( |D| / |{d E D: t E d}|)
+        //El idf es el logaritmo en alguna base de la cantidad total de documentos entre la cantidad de documentos que contienen el termino
+        //Por cada termino:
+        for(int i=0;i<Coleccion.UniqueTermsCount;++i){
+            Coleccion._idf[i] = Math.Log2(((double)Coleccion.Count) / ((double)(1.0 + Coleccion.EnCuantosDocumentosAparece(Coleccion._terminos[i]))));
+        }
+
+        /*//Debugging
+        System.Console.WriteLine("-----DEBUGGING------");
+        System.Console.WriteLine($"La coleccion tiene {Coleccion.Count} documentos");
+        System.Console.WriteLine($"La coleccion tiene {Coleccion.UniqueTermsCount} terminos unicos");
+        StreamWriter newFile = new StreamWriter("../Documentacion/DistintasPalabras.txt");
+        Coleccion._terminos = Coleccion._terminos.Order().ToArray();
+        foreach(string s in Coleccion.Terminos)newFile.WriteLine(s);
+        newFile.Close();
+        System.Console.WriteLine("-----END-----DEBUGGING------");
+        */
     }
 
     //Devuelve el documento en determinada posicion de la coleccion
     public static Documento At(int index){
         if(index < 0 || index >= Coleccion.Count)throw new IndexOutOfRangeException();
 
-        return _documentos[index];
+        return Coleccion._documentos[index];
+    }
+    //Cantidad de documentos diferentes de la coleccion donde aparece este termino
+    public static int EnCuantosDocumentosAparece(string termino){
+        if(string.IsNullOrEmpty(termino) || !Coleccion._terminosYApariciones.ContainsKey(termino))return 0;
+        return Coleccion._terminosYApariciones[termino].Count;
+    }
+    //Documentos de la coleccion donde aparece este termino, arreglo vacio si no aparece
+    public static int[] EnCualesDocumentosAparece(string termino){
+        if(string.IsNullOrEmpty(termino) || !Coleccion._terminosYApariciones.ContainsKey(termino))return new int[0];
+        return Coleccion._terminosYApariciones[termino].ToArray();
     }
 }
