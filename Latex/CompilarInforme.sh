0afc4c63399884e38fba0ae2b02f9ce15409698f
vextroyer <victormanuel.vena@gmail.com>
2023-07-19 15:12:23 -0400

    Reestructurar el proyecto

--- Latex/CompilarInforme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Puede necesitar mas de una compilacion
-pdflatex Informe.tex
-
-#Copia el pdf generado por el comando anterior al directorio raiz del proyecto moogle!
-#Asume que el comando se ejecuta en la ruta (alguna ruta ...)/moogle!/Latex
-path="$PWD/.."
-cp Informe.pdf "$path/Informe.pdf" 
--- Latex/CompilarPresentacion.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Puede necesitar mas de una compilacion
-pdflatex Presentacion.tex
-
-#Copia el pdf generado por el comando anterior al directorio raiz del proyecto moogle!
-#Asume que el comando se ejecuta en la ruta (alguna ruta ...)/moogle!/Latex
-path="$PWD/.."
-cp Presentacion.pdf "$path/Presentacion.pdf" 
